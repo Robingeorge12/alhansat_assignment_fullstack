3c1750c4852fd115fd431551ad3c6f6c0a5fc26d
Robin George <robingsa.btch@gmail.com>
2023-09-06 18:08:42 +0530

    frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import AllRoute from './Route/AllRoute';
+import Navbar from './Components/Navbar/Navbar';
+
+function App() {
+  return (
+    <div className="App">
+  <Navbar />
+  <AllRoute />
+    </div>
+  );
+}
+
+export default App;
